8c75c3ba220d34f0e0eb9a121e93bf1542c82f26
Daniel Chan <dhnchan@gmail.com>
2025-12-24 16:55:39 +0100

    Initial Commit

---
+++ MisotoTests/MisotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MisotoTests.swift
+//  MisotoTests
+//
+//  Created by Daniel Chan on 24.12.2025.
+//
+
+import Testing
+@testable import Misoto
+
+struct MisotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
